9ebede3c7d03d1866aac1331db0306cb66615e2d
Mahesh Ravi <mahesh.r@gosite.com>
2022-06-07 18:01:42 +0530

    initial commit for addToCart sample

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import { IconButton } from '@mui/material';
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  margin: 40px;
+`;
+
+export const Styledbutton = styled(IconButton)`
+  position: fixed;
+  z-index: 100;
+  right: 10px;
+  top:20px;
+`
